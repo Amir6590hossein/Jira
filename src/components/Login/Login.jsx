3fe229820c9212132feeee600e51f7f13fab7329
Amir6590hossein <amirhosseinamjadian1382@gmail.com>
2023-09-12 17:29:09 +0330

    first step  to complete store and redux

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function Login() {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <input
+        type="email"
+        placeholder="email"
+        aria-describedby="email"
+        name="email"
+      ></input>
+      <input
+        type="password"
+        placeholder="password"
+        name="password"
+        aria-describedby="password"
+      ></input>
+    </div>
+  );
 }
